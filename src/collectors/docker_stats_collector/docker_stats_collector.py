a6ce2bc54749d17e7663641b7fe802b808a55285
cszerzo <csabiftw@gmail.com>
2016-03-10 14:16:15 -0500

    Add method for retrieving container name from env var

--- src/collectors/docker_stats_collector/docker_stats_collector.py
+++ src/collectors/docker_stats_collector/docker_stats_collector.py
@@ -34,6 +34,16 @@ class DockerStatsCollector(diamond.collector.Collector):
 			"cpu_stats.cpu_usage.usage_in_kernelmode" : "cpu.kernelmode",
 			"cpu_stats.cpu_usage.usage_in_usermode" : "cpu.usermode",
 			"cpu_stats.system_cpu_usage" : "cpu.system",
+
+			# network stats
+			"networks.eth0.rx_bytes" : "network.rx_bytes",
+			"networks.eth0.rx_packets" : "network.rx_packets",
+			"networks.eth0.rx_errors" : "network.rx_errors",
+			"networks.eth0.rx_dropped" : "network.rx_dropped",
+			"networks.eth0.tx_bytes" : "network.tx_bytes",
+			"networks.eth0.tx_packets" : "network.tx_packets",
+			"networks.eth0.tx_errors" : "network.tx_errors",
+			"networks.eth0.tx_drop" : "network.tx_drop",
 	}
 
 	def get_default_config_help(self):
@@ -85,13 +95,21 @@ class DockerStatsCollector(diamond.collector.Collector):
 
 			# Collect memory and cpu stats
 			for container in running_containers:
+				env_var_name = "JOB_NAME"
+				real_name = "UNKNOWN_NAME"
+				
+				env_vars = client.inspect_container(container["Id"])["Config"]["Env"]
+				try: 
+					real_name = [e.split("=")[1] for e in env_vars if e.startswith(env_var_name)][0]
+				except IndexError, e:
+					pass
 				name = container['Names'][0][1:]
 				s = client.stats(container["Id"])
 				stat = json.loads(s.next())
 				for path in self.METRICS: 
 					val = self.get_value(path, stat)
 					if val is not None:
-						results[".".join([name, self.METRICS.get(path)])] = (val, 'GAUGE')
+						results[".".join([real_name, name, self.METRICS.get(path)])] = (val, 'GAUGE')
 				s.close()
 
 			for name in sorted(results.keys()):
